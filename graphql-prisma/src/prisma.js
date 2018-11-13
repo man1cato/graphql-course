182aed705b7687e1649de9e7bc584c6f3b744fac
Andres <andresrod21@gmail.com>
2018-11-13 09:03:23 -0500

    Completed Section 5

--- graphql-prisma/src/prisma.js
+++ graphql-prisma/src/prisma.js
@@ -7,15 +7,61 @@ const prisma = new Prisma ({
 
 // methods: prisma.query, prisma.mutation, prisma.subscription, prisma.exists
 
-// prisma.query.users(null, '{ id name posts { id title } }').then((data) => {
-//     console.log(JSON.stringify(data, undefined, 2))
+
+const createPostForUser = async (authorId, data) => {
+    const userExists = await prisma.exists.User({ id: authorId })
+
+    if (!userExists) {
+        throw new Error('User not found')
+    }
+
+    const post = await prisma.mutation.createPost({
+        data: {
+            ...data,
+            author: {
+                connect: {
+                    id: authorId
+                }
+            }
+        }
+    }, '{ id author { id name email posts { id title published } } }')
+
+    return post.author
+}
+
+// createPostForUser('cjod8j1n3000w0724vyiva6fu', {
+//     title: "Who's the best Joker?",
+//     body: "Include voice actors",
+//     published: true
+// }).then((user) => {
+//     console.log(JSON.stringify(user, undefined, 2))
 // }).catch((e) => {
-//     console.log(e)
+//     console.log(e.message)
 // })
 
-prisma.query.comments(null, '{ id text author { id name }}').then((data) => {
-    console.log(JSON.stringify(data, undefined, 2))
-}).catch((e) => {
-    console.log(e)
-})
 
+const updatePostForUser = async (postId, data) => {
+    const postExists = await prisma.exists.Post({ id: postId })
+
+    if (!postExists) {
+        throw new Error('Post not found')
+    }
+
+    const post = await prisma.mutation.updatePost({
+        where: {
+            id: postId
+        },
+        data
+    }, '{ author { id name email posts { id title published } } }')
+
+    return post.author
+
+}
+
+
+// updatePostForUser('cjofqw0m1003o07247yoao1t0', {
+//     title: "I'll think of something later",
+//     published: false
+// }).then((user) => {
+//     console.log(JSON.stringify(user, undefined, 2))
+// }).catch((e) => console.log(e.message))
