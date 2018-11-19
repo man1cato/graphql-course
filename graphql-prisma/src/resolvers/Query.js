7745802f48508e458f5c366a6c721f360903d16f
man1cato <andresrod21@gmail.com>
2018-11-19 16:26:27 -0500

    Complete Section 7 - Pagination & Sorting

--- graphql-prisma/src/resolvers/Query.js
+++ graphql-prisma/src/resolvers/Query.js
@@ -1,13 +1,49 @@
 import getUserId from "../utils/getUserId";
 
-const Query = {
-    me(parent, args, { prisma, request }, info) {
-        const userId = getUserId(request)
+const Query = {    
+    users(parent, args, { prisma }, info) {
+        const opArgs = {
+            first: args.first,
+            skip: args.skip,
+            after: args.after, 
+            orderBy: args.orderBy
+        }
 
-        return prisma.query.user({
-            where: { 
-                id: userId 
+        if (args.query) {
+            opArgs.where = {
+                name_contains: args.query
             }
+        }
+
+        return prisma.query.users(opArgs, info)
+    },
+    posts(parent, args, { prisma }, info) {
+        const opArgs = {
+            first: args.first,
+            skip: args.skip,
+            after: args.after,
+            orderBy: args.orderBy,
+            where: {
+                published: true
+            }
+        }
+
+        if (args.query) {
+            opArgs.where.OR = [{
+                title_contains: args.query
+            }, {
+                body_contains: args.query
+            }]
+        }
+
+        return prisma.query.posts(opArgs, info)
+    },
+    comments(parent, args, { prisma }, info) {
+        return prisma.query.comments({
+            first: args.first,
+            skip: args.skip,
+            after: args.after,
+            orderBy: args.orderBy
         }, info)
     },
     async post(parent, { id }, { prisma, request }, info) {
@@ -32,21 +68,23 @@ const Query = {
 
         return post
     },
-    users(parent, args, { prisma }, info) {
-        const opArgs = {}
+    me(parent, args, { prisma, request }, info) {
+        const userId = getUserId(request)
 
-        if (args.query) {
-            opArgs.where = {
-                name_contains: args.query
+        return prisma.query.user({
+            where: { 
+                id: userId 
             }
-        }
-
-        return prisma.query.users(opArgs, info)
+        }, info)
     },
-    myPosts(parent, { query }, { prisma, request }, info) {
+    myPosts(parent, args, { prisma, request }, info) {
         const userId = getUserId(request)
 
         const opArgs = {
+            first: args.first,
+            skip: args.skip,
+            after: args.after,
+            orderBy: args.orderBy,
             where: {
                 author: {
                     id: userId
@@ -54,7 +92,7 @@ const Query = {
             }
         }
 
-        if (query) {
+        if (args.query) {
             opArgs.where.OR = [{
                 title_contains: args.query
             }, {
@@ -64,26 +102,7 @@ const Query = {
 
         return prisma.query.posts(opArgs, info)
     },
-    posts(parent, { query }, { prisma }, info) {
-        const opArgs = {
-            where: {
-                published: true
-            }
-        }
-
-        if (query) {
-            opArgs.where.OR = [{
-                title_contains: args.query
-            }, {
-                body_contains: args.query
-            }]
-        }
-
-        return prisma.query.posts(opArgs, info)
-    },
-    comments(parent, args, { prisma }, info) {
-        return prisma.query.comments(null, info)
-    }
+    
 }
 
 export default Query
